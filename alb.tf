a7b294953e5e57c2203514460aa5594f41d2e71b
Baiyuan Wang <bywang@thoughtworks.com>
2019-07-30 17:07:23 +0800

    Set health check timeout to 10

--- alb.tf
+++ alb.tf
@@ -18,7 +18,7 @@ resource "aws_alb_target_group" "app" {
     interval            = "30"
     protocol            = "HTTP"
     matcher             = "200"
-    timeout             = "3"
+    timeout             = "10"
     path                = var.health_check_path
     unhealthy_threshold = "2"
   }
