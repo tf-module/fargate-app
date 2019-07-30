676942c6932dd931815d67bd90caac4f05fc9e9c
Baiyuan Wang <bywang@thoughtworks.com>
2019-07-30 17:02:20 +0800

    Set aws_alb_listener port to 80

--- alb.tf
+++ alb.tf
@@ -27,7 +27,7 @@ resource "aws_alb_target_group" "app" {
 # Redirect all traffic from the ALB to the target group
 resource "aws_alb_listener" "fargate_app" {
   load_balancer_arn = aws_alb.main.id
-  port              = var.app_port
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
